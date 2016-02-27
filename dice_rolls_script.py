e703939eb469a81843e607e64f7b5c5ef324ec61
Karell Veskimeister <Karell Veskimeister>
2016-02-27 15:25:20 +0200

    fixed casing and added different input method and sum

--- dice_rolls_script.py
+++ dice_rolls_script.py
@@ -3,19 +3,21 @@ from random import randint
 
 def intro():
     print("Enter the amount of times you wish to roll the dice and how many sides the dice has")
-    print("Sample input \"2 4\" Would produce : (3, 1)")
+    print("Sample input \"2d4\" Would produce : (3, 1) the sum would be 4")
     print("Enter input : ")
-    return input().split()
+    return input().lower().split("d")
 
 
-def generate_rolls(rolls_nr, sides):
+def generate_rolls(rollsNr, sides):
     answer = tuple()
-    while rolls_nr > 0:
+    while (rollsNr > 0):
         answer = answer + (randint(1, sides),)
-        rolls_nr -= 1
+        rollsNr -= 1
     return answer
 
 
 if __name__ == '__main__':
     x = intro()
-    print(generate_rolls(int(x[0]), int(x[1])))
+    x = tuple(map(int, x))
+    answer = generate_rolls(x[0], x[1])
+    print(str(answer) + " the sum would be " + str(sum(answer)))
